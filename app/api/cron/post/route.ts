7669c17f7ae21671725037f864a782d6bd7c13ca
kaoru <k-kor.musci1218@outlook.com>
2026-05-12 20:38:55 +0900

    fix cron

--- app/api/cron/post/route.ts
+++ app/api/cron/post/route.ts
@@ -6,36 +6,67 @@ export async function GET() {
   try {
     const posts = await prisma.post.findMany({
       where: {
-  status: "scheduled",
-  scheduledAt: {
-    lte: new Date(),
-  },
-},
+        status: "scheduled",
+        scheduledAt: {
+          lte: new Date(),
+        },
+      },
       include: {
         account: true,
       },
     });
 
+    console.log("SCHEDULED POSTS");
+    console.log(posts);
+
     for (const post of posts) {
       try {
+        console.log("CRON POST");
+
+        console.log({
+          postId: post.id,
+          username:
+            post.account.username,
+          token:
+            post.account
+              .xAccessToken,
+        });
+
         if (
           !post.account.xApiKey ||
           !post.account.xApiSecret ||
           !post.account.xAccessToken ||
           !post.account.xAccessSecret
         ) {
-          
           continue;
         }
 
         const client = new TwitterApi({
-          appKey: post.account.xApiKey,
-          appSecret: post.account.xApiSecret,
-          accessToken: post.account.xAccessToken,
-          accessSecret: post.account.xAccessSecret,
+          appKey:
+            post.account.xApiKey,
+
+          appSecret:
+            post.account.xApiSecret,
+
+          accessToken:
+            post.account
+              .xAccessToken,
+
+          accessSecret:
+            post.account
+              .xAccessSecret,
         }).readWrite;
 
-        await client.v2.tweet(post.content);
+        const me =
+          await client.v2.me();
+
+        console.log("ME");
+
+        console.log(me);
+
+        await client.v2.tweet(
+          post.content
+        );
 
         await prisma.post.update({
           where: {
@@ -47,7 +78,11 @@ export async function GET() {
           },
         });
       } catch (error) {
-        console.error("予約投稿失敗:", post.id, error);
+        console.error(
+          "予約投稿失敗:",
+          post.id,
+          error
+        );
       }
     }
 
